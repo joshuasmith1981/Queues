8ca1bb22b20fe73bc2b065ab308d502e88e00b59
xo8o <joshua.l.smith@nordstrom.com>
2014-02-17 15:53:54 -0800

    Implemented RandomizedQueue

--- src/RandomizedQueue.java
+++ src/RandomizedQueue.java
@@ -11,6 +11,21 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 	 */
 	public static void main(String[] args) {
 
+		RandomizedQueue<Integer> queue = new RandomizedQueue<Integer>();
+		
+		for (int i = 0; i < 5; i++) {
+			queue.enqueue(Integer.valueOf(i));
+		}
+		
+		for (int i = 0; i < 5; i++) {
+			Integer result = queue.dequeue();
+			
+			if (result != null) {
+				System.out.println(String.format("Dequeued value: %s", result));
+			} else {
+				System.out.println("Dequeue() returned null value.");
+			}
+		}		
 	}
 	
 	public RandomizedQueue() {
@@ -22,7 +37,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 	}
 	
 	public int size() {
-		return items.length;				
+		return N;				
 	}
 	
 	public void enqueue(Item item) {
@@ -30,29 +45,49 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 			resizeArray(items.length * 2);
 		}
 		
-		items[N++] = item;
+		items[N++] = item;		
 	}
 	
 	public Item dequeue() {
-		// TODO: delete and return a random item		
+		
+		if (N == 0) {
+			throw new NoSuchElementException();
+		}
+		
+		if (N == 1) {
+			Item lastItem = items[0];
+			items[0] = null;
+			N--;
+			
+			return lastItem;
+		}
 	
-		int randomIndex = StdRandom.uniform(0, N);
+		int randomIndex = StdRandom.uniform(0, N - 1);		
 		
-		Item item = items[randomIndex];
+		Item item = items[randomIndex];		
+		items[randomIndex] = null;
 		
-		N--;
+		N--;		
+		
+		if (randomIndex < N) {
+			compressArray(randomIndex + 1);
+		}
 		
 		if (N > 0 && N == items.length / 4) {
 			resizeArray(items.length / 2);
 		}
 		
-		return null;
+		return item;
 	}
 	
 	/**
 	 * Returns (but does not delete) a random item
 	 */
 	public Item sample() {
+		
+		if (N == 0) {
+			throw new NoSuchElementException();
+		}
 		
 		int randomIndex = StdRandom.uniform(0, N);
 		
@@ -63,7 +98,9 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 	   return new RandomIterator();
 	}
 	
-	private void resizeArray(int size) {
+	private void resizeArray(int size) {		
+		
+		@SuppressWarnings("unchecked")
 		Item[] newArray = (Item[]) new Object[size];
 		
 		for (int i = 0; i < N; i++) {
@@ -71,6 +108,18 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 		}
 		
 		items = newArray;
+	}
+	
+	private void compressArray(int startingPosition) {
+	
+		for (int i = startingPosition; i < items.length; i++) {
+			Item currentItem = items[i];
+			
+			if (currentItem != null) {					
+				items[i - 1] = currentItem;
+				items[i] = null;
+			}
+		}
 	}
 	   
 	private class RandomIterator implements Iterator<Item> {
