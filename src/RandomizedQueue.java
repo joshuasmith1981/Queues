03588093a7648df5b10ec8a316f473d193a0d70d
Joshua Smith <joshua.l.smith@nordstrom.com>
2014-02-18 16:25:02 -0800

    Implemented iterator()

--- src/RandomizedQueue.java
+++ src/RandomizedQueue.java
@@ -2,137 +2,167 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
-	
+
 	private Item[] items;
 	private int N = 0;
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 
 		RandomizedQueue<Integer> queue = new RandomizedQueue<Integer>();
+
+		int count = 10000;
 		
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < count; i++) {
 			queue.enqueue(Integer.valueOf(i));
 		}
 		
-		for (int i = 0; i < 5; i++) {
-			Integer result = queue.dequeue();
-			
-			if (result != null) {
-				System.out.println(String.format("Dequeued value: %s", result));
-			} else {
-				System.out.println("Dequeue() returned null value.");
-			}
-		}		
+		for (int i = 0; i < count; i++) {
+			System.out.println(String.format("Dequeued: %s", queue.dequeue()));
+		}
 	}
-	
+
 	public RandomizedQueue() {
-		items = (Item[]) new Object[1];	
+		items = (Item[]) new Object[1];
 	}
-	
+
 	public boolean isEmpty() {
 		return N == 0;
 	}
-	
+
 	public int size() {
-		return N;				
+		return N;
 	}
-	
+
 	public void enqueue(Item item) {
 		if (N == items.length - 1) {
 			resizeArray(items.length * 2);
 		}
-		
-		items[N++] = item;		
+
+		items[N++] = item;
 	}
-	
+
 	public Item dequeue() {
-		
+
 		if (N == 0) {
 			throw new NoSuchElementException();
 		}
-		
+
 		if (N == 1) {
 			Item lastItem = items[0];
 			items[0] = null;
 			N--;
-			
+
 			return lastItem;
 		}
-	
-		int randomIndex = StdRandom.uniform(0, N - 1);		
-		
-		Item item = items[randomIndex];		
+
+		int randomIndex = StdRandom.uniform(0, N - 1);
+
+		Item item = items[randomIndex];
 		items[randomIndex] = null;
-		
-		N--;		
-		
+
+		N--;
+
 		if (randomIndex < N) {
 			compressArray(randomIndex + 1);
 		}
-		
+
 		if (N > 0 && N == items.length / 4) {
 			resizeArray(items.length / 2);
 		}
-		
+
 		return item;
 	}
-	
+
 	/**
 	 * Returns (but does not delete) a random item
 	 */
 	public Item sample() {
-		
+
 		if (N == 0) {
 			throw new NoSuchElementException();
 		}
-		
+
 		int randomIndex = StdRandom.uniform(0, N);
-		
+
 		return items[randomIndex];
 	}
-	
+
 	public Iterator<Item> iterator() {
-	   return new RandomIterator();
+		return new RandomIterator();
 	}
-	
-	private void resizeArray(int size) {		
-		
+
+	private void resizeArray(int size) {
+
 		@SuppressWarnings("unchecked")
 		Item[] newArray = (Item[]) new Object[size];
-		
+
 		for (int i = 0; i < N; i++) {
 			newArray[i] = items[i];
 		}
-		
+
 		items = newArray;
 	}
-	
+
 	private void compressArray(int startingPosition) {
-	
+
 		for (int i = startingPosition; i < items.length; i++) {
 			Item currentItem = items[i];
-			
-			if (currentItem != null) {					
+
+			if (currentItem != null) {
 				items[i - 1] = currentItem;
 				items[i] = null;
 			}
 		}
 	}
-	   
+
 	private class RandomIterator implements Iterator<Item> {
+
+		private int[] indices;
+		private int currentIndex = 0;
+
+		public RandomIterator() {
+			indices = new int[N];
+
+			for (int i = 0; i < N; i++) {
+				
+				boolean foundNewValue = false; 
+				
+				while (!foundNewValue) {
+					int value = StdRandom.uniform(0, N);
+					
+					if (!valueAlreadyGenerated(value, i)) {
+						indices[i] = value;
+						foundNewValue = true;
+					}
+				}
+			}
+		}
+
 		public boolean hasNext() {
+			return currentIndex < indices.length;
+		}
+
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+
+		public Item next() {
+			int index = indices[currentIndex++];
+			
+			return items[index];
+		}
+		
+		private boolean valueAlreadyGenerated(int value, int currentPosition) {
+			for (int i = 0; i < currentPosition; i++) {
+				if (indices[i] == value) { 
+					return true;
+				}
+			}
+			
 			return false;
-	    }
-	   
-	    public void remove() {
-	    	throw new UnsupportedOperationException();
-	    }
-	   
-	    public Item next() {
-	    	return null;
-	    }
-   }
+		}
+	}
 }
